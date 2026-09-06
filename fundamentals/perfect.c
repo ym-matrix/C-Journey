51014d474cb3f231371eea24582d526921592816
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-06 14:12:52 +0530

    Added perfect number program

---
+++ fundamentals/perfect.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int i, n, sum = 0, temp;
+    printf("Enter a number:");
+    scanf("%d", &n);
+    temp = n;
+
+    if (n < 0)
+    {
+        temp = -n;
+    }
+
+    for (i = 1; i < temp; i++)
+    {
+        if (temp % i == 0)
+        {
+            sum = sum + i;
+        }
+    }
+    if (sum == temp)
+    {
+        printf("The given number %d is a perfect number:", temp);
+    }
+    else
+    {
+        printf("The given number %d is not a perfect number:", temp);
+    }
+    return 0;
+}
