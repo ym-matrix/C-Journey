366e371bc9ec6a1c4094119de27933ebc70844e8
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-09 21:39:17 +0530

    Added comments to 3rd pattern program

--- fundamentals/pattern2.c
+++ fundamentals/pattern2.c
@@ -2,14 +2,14 @@
 int main()
 {
     int i, j, n;
-    printf("Enter a number for rows and columns:");
+    printf("Enter a number for rows and columns:"); // input number of rows and columns
     scanf("%d", &n);
 
-    for (i = 1; i <= n; i++)
+    for (i = 1; i <= n; i++) // outer loop tp execute the number of rows
     {
-        for (j = 0; j < i; j++)
+        for (j = 0; j < i; j++) // inner loop to execute the number of columns
         {
-            if ((i + j) % 2 != 0)
+            if ((i + j) % 2 != 0) // if sum of i and j is divisible by 2 then print 1
             {
                 printf("1");
             }
