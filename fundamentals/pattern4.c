8b56d1b6ed46b9f95d405f339f7f77f440d28bb0
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-09 22:04:07 +0530

    Made Loop condition changes

--- fundamentals/pattern4.c
+++ fundamentals/pattern4.c
@@ -2,16 +2,16 @@
 int main()
 {
     int i, j, rows, col;
-    printf("Enter number of rows:");
+    printf("Enter number of rows:"); // input the number of rows
     scanf("%d", &rows);
-    printf("Enter number of columns:");
+    printf("Enter number of columns:"); // input the number of columns
     scanf("%d", &col);
 
-    for (i = 1; i <= rows; i++)
+    for (i = 1; i <= rows; i++) // outer loop for executing the number of rows
     {
-        for (j = 1; j <= col; j++)
+        for (j = 1; j <= col; j++) // inner loop for executing the number of columns
         {
-            if (i == 1 || j == 1 || i == rows || j == col)
+            if (i == 1 || j == 1 || i == rows || j == col) // condition to print 1
             {
                 printf("1");
             }
