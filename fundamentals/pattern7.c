aec2b8ee64a1493aeb6436e836788f193ed7bd29
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-10 20:35:14 +0530

    Added comments to pattern program

--- fundamentals/pattern7.c
+++ fundamentals/pattern7.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
+// Program to print an inverted triangle pattern of stars
 int main(void)
 {
     int i, j, k, rows;
 
+    // Read the number of rows
     printf("Enter number of rows: ");
     scanf("%d", &rows);
 
+    // Print rows from the largest to the smallest
     for (i = rows; i >= 1; i--)
     {
+        // Print leading spaces to align the triangle
         for (j = 0; j < rows - i; j++)
         {
             printf(" ");
         }
 
+        // Print stars in the current row
         for (k = 1; k <= 2 * i - 1; k++)
         {
             printf("*");
