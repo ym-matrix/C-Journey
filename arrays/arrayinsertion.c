075ae16a9eedec8923b90a87dc878569dca5cab1
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-16 17:41:39 +0530

    Made loop condition changes

--- arrays/arrayinsertion.c
+++ arrays/arrayinsertion.c
@@ -4,6 +4,7 @@ int main()
     int i, n, element, pos;
     printf("Enter size of array:");
     scanf("%d", &n);
+    // declaring a variable of n+1 size
     int arr[n + 1];
 
     printf("Enter values inside array");
@@ -17,12 +18,14 @@ int main()
     printf("Enter position:");
     scanf("%d", &pos);
 
+    // printing previous array before modifying
     printf("\nprevious array:");
     for (i = 0; i < n; i++)
     {
         printf("%d", arr[i]);
     }
 
+    // if pos entered is not in the array
     if (pos > n + 1 || pos < 0)
     {
         printf("Enter valid position");
@@ -31,6 +34,7 @@ int main()
     {
         for (i = n; i >= pos; i--)
         {
+            // moving elements towards right side to make space
             arr[i] = arr[i - 1];
         }
         arr[pos - 1] = element;
@@ -38,9 +42,10 @@ int main()
     }
 
     printf("\n new array:");
-
+    // printing new array after adding values
     for (i = 0; i < n; i++)
     {
         printf("%d", arr[i]);
     }
+    return 0;
 }
