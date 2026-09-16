b132b57f8b39ae55f20f04946fb8912694d40fb6
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-16 20:33:57 +0530

    Added linear search program

---
+++ arrays/linearsearch.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    int i, n, element, result;
+    printf("Enter size of array:");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("Enter values inside array:");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter an element to find:");
+    scanf("%d", &element);
+
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == element)
+        {
+            result = i;
+            break;
+        }
+    }
+    if (result == -1)
+    {
+        printf("Value not found in array:");
+    }
+    else
+    {
+        printf("Value found at index %d", result);
+    }
+    return 0;
+}
