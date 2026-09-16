ae4ed105ec9bffeb911581234ae0a43c37995ca3
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-16 20:37:24 +0530

    Added 9th pattern program

---
+++ fundamentals/pattern9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, rows, col;
+    printf("Enter number of rows:");
+    scanf("%d", &rows);
+    printf("Enter number of columns:");
+    scanf("%d", &col);
+
+    for (i = 0; i <= rows; i++)
+}
