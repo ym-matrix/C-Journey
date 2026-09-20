51b887ddcd55ca6056725089c1e315ec9c8c8ab5
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-20 18:34:48 +0530

    added comments

--- arrays/bubblesort2.c
+++ arrays/bubblesort2.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
+
+// Bubble sort program to sort an array in descending order
 int main()
 {
     int i, j, n, temp;
+
+    // Read the size of the array
     printf("Enter size of array:");
     scanf("%d", &n);
     int arr[n];
-    printf("Enter values inside array:");
 
+    // Read array elements from the user
+    printf("Enter values inside array:");
     for (i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
 
+    // Perform bubble sort for descending order
     for (i = 0; i < n - 1; i++)
     {
+        // Compare adjacent values and swap if left is smaller than right
         for (j = 0; j < n - i - 1; j++)
         {
             if (arr[j] < arr[j + 1])
@@ -24,6 +31,8 @@ int main()
             }
         }
     }
+
+    // Print the sorted array
     printf("Sorted array:");
     for (i = 0; i < n; i++)
     {
