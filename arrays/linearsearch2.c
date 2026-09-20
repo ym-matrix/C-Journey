e5871601302297b66b1f9e182c03fa9032f2c5bd
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-20 18:47:25 +0530

    Added comments

--- arrays/linearsearch2.c
+++ arrays/linearsearch2.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+
+// Linear search program to find an element in an array
 int main()
 {
     int i, n, element, result;
+
+    // Read array size
     printf("Enter size of array:");
     scanf("%d", &n);
     int arr[n];
 
+    // Read array elements
     printf("Enter values inside array:");
     for (i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
+
+    // Read the value to search for
     printf("Enter an element to be searched:");
     scanf("%d", &element);
 
+    // Search from the first element to the last
     for (i = 0; i < n; i++)
     {
         if (arr[i] == element)
@@ -22,6 +30,8 @@ int main()
             break;
         }
     }
+
+    // Display result based on whether the element was found
     if (result == -1)
     {
         printf("The element was not found in the array:");
