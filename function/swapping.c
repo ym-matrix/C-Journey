d6ac4b880e14583b000d94b353e2e15ab87671d6
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-20 17:26:49 +0530

    Added comments

--- function/swapping.c
+++ function/swapping.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
+
+// Function to swap two integer values using pointers
 void swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+    int temp = *a; // store first value temporarily
+    *a = *b;       // assign second value to first pointer
+    *b = temp;     // assign saved first value to second pointer
 }
+
 int main()
 {
     int x = 5, y = 15;
+
     printf("Variables before swapping\n");
     printf("x=%d,y=%d\n", x, y);
 
-    swap(&x, &y);
+    swap(&x, &y); // pass addresses of x and y
+
     printf("Variables after swapping\n");
     printf("x=%d,y=%d\n", x, y);
 
