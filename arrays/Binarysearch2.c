c0b1726ae9dbc838e64adda96aef727fd98aa553
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-21 22:32:50 +0530

    added Binary search program for descending order

---
+++ arrays/Binarysearch2.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, j, temp, element, low, high, mid, found = 0;
+    printf("Enter size of array:");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements:");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] < arr[j + 1])
+            {
+                temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    printf("\n");
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    printf("Enter the number to search:");
+    scanf("%d", &element);
+
+    low = 0;
+    high = n - 1;
+
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if (arr[mid] == element)
+        {
+            found = 1;
+            break;
+        }
+        else if (arr[mid] > element)
+        {
+            low = mid - 1;
+        }
+        else
+        {
+
+            high = mid + 1;
+        }
+    }
+
+    if (found)
+    {
+        printf("Element found at index %d\n", mid);
+    }
+    else
+    {
+        printf("Element not found\n");
+    }
+    return 0;
+}
