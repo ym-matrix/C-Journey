79add282fc37644639116f8f58e65fefd698e304
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-23 21:45:04 +0530

    Added comments

--- arrays/Identitymatrix.c
+++ arrays/Identitymatrix.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+
+// Program to check whether a square matrix is an identity matrix
 int main()
 {
     int n, i, j, isIdentity = 1;
 
+    // Read the size of the square matrix
     printf("Enter size of square matrix (n x n):");
     scanf("%d", &n);
 
-    int[n][n];
+    int matrix[n][n];
 
+    // Read matrix elements
     printf("Enter matrix elements:\n");
     for (i = 0; i < n; i++)
     {
@@ -16,6 +20,8 @@ int main()
             scanf("%d", &matrix[i][j]);
         }
     }
+
+    // Check every element: diagonal must be 1 and non-diagonal must be 0
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < n; j++)
@@ -37,6 +43,7 @@ int main()
         }
     }
 
+    // Print the final result
     if (isIdentity)
     {
         printf("The given matrix is an Identity Matrix.\n");
