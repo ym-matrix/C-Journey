5a629de092a52992d03a070a8785604ee27291a8
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-23 21:38:40 +0530

    Added Identity check program

--- arrays/Identitymatrix.c
+++ arrays/Identitymatrix.c
@@ -1,4 +1,50 @@
 #include <stdio.h>
 int main()
 {
+    int n, i, j, isIdentity = 1;
+
+    printf("Enter size of square matrix (n x n):");
+    scanf("%d", &n);
+
+    int[n][n];
+
+    printf("Enter matrix elements:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                if (matrix[i][j] != 1)
+                {
+                    isIdentity = 0;
+                }
+            }
+            else
+            {
+                if (matrix[i][j] != 0)
+                {
+                    isIdentity = 0;
+                }
+            }
+        }
+    }
+
+    if (isIdentity)
+    {
+        printf("The given matrix is an Identity Matrix.\n");
+    }
+    else
+    {
+        printf("The given matrix is NOT an Identity Matrix.\n");
+    }
+
+    return 0;
 }
